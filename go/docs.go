bd13e3afe4eae8de579a63a296012e6fcc9fae38
peugeot <thomaspeugeot@yahoo.fr>
2022-04-02 04:35:32 +0200

    initial commit

---
+++ go/docs.go
@@ -0,0 +1,24 @@
+// Package gongng2charts gongng2charts backend API
+//
+// gongng2charts exposes types of a package.
+//
+//     Schemes: http
+//     BasePath: /api/github.com/fullstack-lang/gongng2charts/go/v1
+//     Version: 0.1
+//     Host: localhost:8080
+//
+//     Consumes:
+//     - application/json
+//
+//     Produces:
+//     - application/json
+//
+//     Security:
+//     - basic
+//
+//    SecurityDefinitions:
+//    basic:
+//      type: basic
+//
+// swagger:meta
+package gongng2charts
