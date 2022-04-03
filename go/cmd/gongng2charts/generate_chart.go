b8abf8c78f438bbbae67f8ab6ab30f3fd271451b
peugeot <thomaspeugeot@yahoo.fr>
2022-04-03 07:21:57 +0200

    deal with multi charts Fixes #1

--- go/cmd/gongng2charts/generate_chart.go
+++ go/cmd/gongng2charts/generate_chart.go
@@ -7,12 +7,17 @@ import (
 	gongng2charts_models "github.com/fullstack-lang/gongng2charts/go/models"
 )
 
-const NbPoints = 20
-const NbDatasets = 4
+const NbPoints = 5
+const NbDatasets = 2
 
 func GenerateChart() {
+	GenerateChart1()
+	GenerateChart2()
+	gongng2charts_models.Stage.Commit()
+}
+func GenerateChart1() {
 
-	chartConfig := (&gongng2charts_models.ChartConfiguration{Name: "Dummy"}).Stage()
+	chartConfig := (&gongng2charts_models.ChartConfiguration{Name: "Chart 1"}).Stage()
 	chartConfig.ChartType = gongng2charts_models.LINE
 
 	for idx_dataset := 0; idx_dataset < NbDatasets; idx_dataset = idx_dataset + 1 {
@@ -32,6 +37,28 @@ func GenerateChart() {
 		label := (&gongng2charts_models.Label{Name: fmt.Sprintf("Month %d", idx)}).Stage()
 		chartConfig.Labels = append(chartConfig.Labels, label)
 	}
+}
 
-	gongng2charts_models.Stage.Commit()
+func GenerateChart2() {
+
+	chartConfig := (&gongng2charts_models.ChartConfiguration{Name: "Chart 2"}).Stage()
+	chartConfig.ChartType = gongng2charts_models.BAR
+
+	for idx_dataset := 0; idx_dataset < NbDatasets; idx_dataset = idx_dataset + 1 {
+		dataset := (&gongng2charts_models.Dataset{Name: fmt.Sprintf("Dataset 2 %d", 0)}).Stage()
+		chartConfig.Datasets = append(chartConfig.Datasets, dataset)
+
+		for idx := 0; idx < NbPoints; idx = idx + 1 {
+			datapoint := (&gongng2charts_models.DataPoint{Name: fmt.Sprintf("Day %d", idx)}).Stage()
+			datapoint.Value = rand.Float64() * 30.0
+			dataset.DataPoints = append(dataset.DataPoints, datapoint)
+		}
+
+		dataset.Label = fmt.Sprintf("set %d", idx_dataset)
+	}
+
+	for idx := 0; idx < NbPoints; idx = idx + 1 {
+		label := (&gongng2charts_models.Label{Name: fmt.Sprintf("Day %d", idx)}).Stage()
+		chartConfig.Labels = append(chartConfig.Labels, label)
+	}
 }
